2b52400da1239cc9f2238fb8ebc2abf310b0ae85
oleksandr_haidaiev <gaidaev.av@gmail.com>
2023-04-15 21:36:16 +0300

    Created empty project

---
+++ Radio/RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioApp.swift
+//  Radio
+//
+//  Created by Oleksandr Haidaiev on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
