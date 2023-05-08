c78f69b50ae1c40b9d9220e44e714a5f3f4e187e
oleksandr_haidaiev <gaidaev.av@gmail.com>
2023-05-08 16:28:09 +0300

    Changes related to PlayerDetailsViewModel (not final)

--- Radio/Common screens/PlayAudioScreen/PlayerDetailsViewModel.swift
+++ Radio/Common screens/PlayAudioScreen/PlayerDetailsViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Oleksandr Haidaiev on 23.04.2023.
 //
 
+import Foundation
 import Combine
 
 class PlayerDetailsViewModel: ObservableObject, GeneralViewModel {
@@ -14,21 +15,44 @@ class PlayerDetailsViewModel: ObservableObject, GeneralViewModel {
         case download
     }
     
+    enum DownloadUIState {
+        case unavailable
+        case alreadyDownloaded
+        case downloadState(Loadable<DownloadRepository.DownloadState,
+                           DownloadRepository.RepositoryError>)
+    }
+    
     let audioItem:  Model.SearchDataAudioItem
     
     @Published private(set) var isPlaying = false
-        
+    @Published private(set) var downloadUIState: DownloadUIState // TODO: update UI with it
+    
     private let di: DIContainer
+    private let offlineMediaFilesDP: any OfflineMediaFilesDataProviding
     private var cancellable = AnyCancellableSet()
     
-    init(audioItem: Model.SearchDataAudioItem, di: DIContainer) {
+    init(audioItem: Model.SearchDataAudioItem,
+         di: DIContainer,
+         offlineMediaFilesDP: (any OfflineMediaFilesDataProviding)? = nil) {
         self.audioItem = audioItem
         self.di = di
+        self.offlineMediaFilesDP = offlineMediaFilesDP ??
+        di.dataProvidersFactory.createOfflineMediaFilesDP()
 
+        if "audioItem.downloadAvailable".count > 3, let url = audioItem.url { // emulation
+            if self.offlineMediaFilesDP.isFileDownloaded(for: url) {
+                downloadUIState = .alreadyDownloaded
+            } else {
+                downloadUIState = .downloadState(.readyToStart)
+            }
+        } else {
+            downloadUIState = .unavailable
+        }
+        
         di.appState
             .publisher(for: \.sharedData.audioPlayerData)
             .sink { [weak self] newPlayerData in
-                self?.checkState(for: newPlayerData)
+                self?.checkPlayingState(for: newPlayerData)
             }
             .store(in: &cancellable)
     }
@@ -47,15 +71,28 @@ class PlayerDetailsViewModel: ObservableObject, GeneralViewModel {
                 di.appState[\.sharedData].audioPlayerData.isPlaying.toggle()
             }
         case .download:
-            break
+            guard let url = audioItem.url else { return }
+            download(url: url)
         }
     }
     
-    private func checkState(for playerData: AppState.SharedData.AudioPlayerData) {
+    private func checkPlayingState(for playerData: AppState.SharedData.AudioPlayerData) {
         if playerData.itemToPlay?.url == audioItem.url {
             isPlaying = playerData.isPlaying
         } else {
             isPlaying = false
         }
+    }
+    
+    private func download(url: URL) {
+        let container = CancellableContainer()
+        downloadUIState = .downloadState(.loadingInProgress(nil, container))
+        
+        offlineMediaFilesDP
+            .downloadFile(for: url)
+            .sinkWithLoadable { [weak self] newValue in
+                self?.downloadUIState = .downloadState(newValue)
+            }
+            .store(in: &container.cancellable)
     }
 }
